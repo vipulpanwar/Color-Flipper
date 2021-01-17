e8740c07bec7b9d2e6280938768813e35d004f94
Vipul <vipul19panwar@gmail.com>
2021-01-17 13:09:18 +0530

    Completed setup for simple color flipper

---
+++ app.js
@@ -0,0 +1,16 @@
+const colors = ["green", "red", "rgba(133,122,200)", "blue", "yellow", "pink", "#f15025"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+    const randomNumber = getRandomNumber();  // getting random index value of colors array
+    console.log(randomNumber);  // to check if no's are not in points
+
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+
+});
+
+function getRandomNumber() {
+    return Math.floor(Math.random()*colors.length);
+}
